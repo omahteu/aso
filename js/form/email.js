4eaa43406c03e56e3fbe17115de41b3ae953ee9c
matheus <matheus.mmn@hotmail.com>
2024-07-04 07:34:35 -0300

    adicionado campo de configurações de email emissor

---
+++ js/form/email.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('form').submit(function(event) {
+      event.preventDefault();
+  
+      // Define um array para armazenar os nomes dos campos que queremos capturar
+      var campos = [
+        'email', // Nome Fantasia
+      ];
+  
+      var empresa = {};
+  
+      // Itera sobre os campos definidos e captura os valores
+      campos.forEach(function(campoId) {
+        empresa[campoId] = $('#' + campoId).val();
+      });
+  
+      var email = JSON.parse(localStorage.getItem('email')) || [];
+      email.push(empresa);
+      localStorage.setItem('email', JSON.stringify(email));
+  
+      
+  
+      alert('Email emissor alterado com sucesso!');
+
+      $('form')[0].reset();
+      location.reload();
+    });
+  });
+  
