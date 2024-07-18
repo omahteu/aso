140431604b2d470aaac08d5d925af0df665e9b6f
matheus <matheus.mmn@hotmail.com>
2024-07-18 03:33:17 -0300

    ajustes diversos

--- js/table/credenciadas.js
+++ js/table/credenciadas.js
@@ -13,11 +13,8 @@ $(document).ready(function () {
         // Itera sobre os dados das credenciadas e cria as linhas da tabela
         credenciadas.forEach(function (empresa, index) {
             var row = '<tr>' +
-                '<td>' + empresa.unidade + '</td>' +
                 '<td>' + empresa.empresa + '</td>' +
                 '<td>' + empresa.nome + '</td>' +
-                '<td>' + empresa.ie + '</td>' +
-                '<td>' + empresa.matricula + '</td>' +
                 '<td>' + empresa.cnpj + '</td>' +
                 '<td>' + empresa.telefone + '</td>' +
                 '<td>' + empresa.email + '</td>' +
@@ -29,6 +26,21 @@ $(document).ready(function () {
                 '<td>' + empresa.cidade + '</td>' +
                 '<td>' + empresa.estado + '</td>' +
                 '<td>' + empresa.observacoes + '</td>' +
+                '<td>' +
+                    '<button type="button" class="btn btn-info">'+
+                        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">'+
+                        '<path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>'+
+                        '</svg>'+
+                    '</button>'+
+                '</td>' +
+                '<td>' +
+                    '<button type="button" class="btn btn-info">'+
+                        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">'+
+                            '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>'+
+                            '<path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>'
+                        '</svg>'+
+                    '</button>'+
+                '</td>' +
                 '</tr>';
             tabela.append(row);
         });
@@ -75,25 +87,36 @@ $(document).ready(function () {
             });
 
             // Insere os dados filtrados na tabela
-            $.each(dadosFiltrados, function(i, item) {
+            $.each(dadosFiltrados, function(i, empresa) {
                 var newRow = '<tr>' +
-                                '<td> <input type="checkbox" name="" id=""> </td>' +
-                                '<td> <input type="checkbox" name="" id=""> </td>' +
-                                '<td>' + item.unidade + '</td>' +
-                                '<td>' + item.empresa + '</td>' +
-                                '<td>' + item.credenciada + '</td>' +
-                                '<td>' + item.nome + '</td>' +
-                                '<td>' + item.rg + '</td>' +
-                                '<td>' + item.cpf + '</td>' +
-                                '<td>' + item.nascimento + '</td>' +
-                                '<td>' + item.sexo + '</td>' +
-                                '<td>' + item.funcao + '</td>' +
-                                '<td>' + item.dataExame + '</td>' +
-                                '<td>' + item.tiposExame + '</td>' +
-                                '<td>' + item.nomesExames + '</td>' +
-                                '<td>' + item.situacao + '</td>' +
-                                '<td>' + item.observacao + '</td>' +
-                                '<td>' + item.pagamento + '</td>' +
+                                '<td>' + empresa.empresa + '</td>' +
+                                '<td>' + empresa.nome + '</td>' +
+                                '<td>' + empresa.cnpj + '</td>' +
+                                '<td>' + empresa.telefone + '</td>' +
+                                '<td>' + empresa.email + '</td>' +
+                                '<td>' + empresa.cep + '</td>' +
+                                '<td>' + empresa.rua + '</td>' +
+                                '<td>' + empresa.numero + '</td>' +
+                                '<td>' + empresa.complemento + '</td>' +
+                                '<td>' + empresa.bairro + '</td>' +
+                                '<td>' + empresa.cidade + '</td>' +
+                                '<td>' + empresa.estado + '</td>' +
+                                '<td>' + empresa.observacoes + '</td>' +
+                                '<td>' +
+                                    '<button type="button" class="btn btn-info">'+
+                                        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">'+
+                                        '<path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>'+
+                                        '</svg>'+
+                                    '</button>'+
+                                '</td>' +
+                                '<td>' +
+                                    '<button type="button" class="btn btn-info">'+
+                                        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">'+
+                                            '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>'+
+                                            '<path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>'
+                                        '</svg>'+
+                                    '</button>'+
+                                '</td>' +
                              '</tr>';
                 $('#tabelaAsos').append(newRow);
             });
